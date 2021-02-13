c1c965228fcee6026604c6748d98f491a24596a8
Dmitry Ovsyanko <do_@pochta.ru>
2021-02-13 19:16:10 +0300

    1st post

---
+++ front/src/main.js
@@ -0,0 +1,15 @@
+import 'devextreme/dist/css/dx.common.css';
+//import './themes/generated/dx.generic.t1.css';
+import './themes/generated/theme1.base.css';
+import './themes/generated/theme1_.base.css';
+import './themes/generated/theme.additional.css';
+import { createApp }  from "vue";
+import router from "./router";
+
+import App from "./App";
+import appInfo from "./app-info";
+
+const app = createApp(App);
+app.use(router);
+app.config.globalProperties.$appInfo = appInfo;
+app.mount('#app');
---
+++ front/src/views/tables.vue
@@ -0,0 +1,72 @@
+<template>
+  <div style="display:flex">
+<!--
+    <h2 class="content-block">Таблицы</h2>
+-->
+    <dx-data-grid
+      class="dx-card wide-card"
+      :allow-column-resizing="true"
+      :data-source="dataSourceConfig"
+      :focused-row-index="0"
+      :show-borders="false"
+      :show-column-lines="true"
+      :show-row-lines="false"
+      :row-alternation-enabled="true"
+      :focused-row-enabled="true"
+      :column-auto-width="true"
+      :column-hiding-enabled="false"
+      :width="'100%'"
+      :height="'100%'"
+    >
+
+      <dx-pager :visible="'true'" :show-info="true" :show-navigation-buttons="0" />
+      <dx-paging :page-size="100" />
+      <dx-scrolling mode="virtual" row-rendering-mode="virtual" />
+
+      <dx-filter-row :visible="true" />
+
+		<dx-column data-field="id"    caption="Имя" />
+		<dx-column data-field="note"  caption="Комментарий" />
+		<dx-column data-field="pk"    caption="ПК" />
+		<dx-column data-field="cnt"   caption="~К-во строк" />
+
+    </dx-data-grid>
+
+  </div>
+</template>
+
+<script>
+import "devextreme/data/odata/store";
+import DxDataGrid, {
+  DxColumn,
+  DxFilterRow,
+  DxLookup,
+  DxPager,
+  DxPaging,
+  DxScrolling
+} from "devextreme-vue/data-grid";
+
+export default {
+  setup() {
+    const dataSourceConfig = {
+      store: {
+        type: "odata",
+        key: "id",
+        url: "/_back/tables"
+      },
+    };
+    return {
+      dataSourceConfig,      
+    };
+  },
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxFilterRow,
+    DxLookup,
+    DxPager,
+    DxPaging,
+    DxScrolling
+  }
+};
+</script>
